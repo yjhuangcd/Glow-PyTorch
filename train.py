eb6f3571f3ced90dfa8cfc2c4f6424317fcf0399
yjhuangcd <yjhuang@caltech.edu>
2019-07-30 16:56:00 -0700

    split latent space

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ from ignite.handlers import ModelCheckpoint, Timer
 from ignite.metrics import RunningAverage, Loss
 
 from datasets import get_CIFAR10, get_SVHN
-from model import Glow
+from model_info import Glow
 
 import logging
 from tensorboardX import SummaryWriter
